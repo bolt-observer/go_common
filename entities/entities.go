beb804d4d41f6a5b157e1d2bc965b2167fb5530f
Gregor Pogacnik <gregor@bolt.observer>
2022-09-15 14:30:49 +0200

    feat: JsonTime

--- entities/entities.go
+++ entities/entities.go
@@ -1,5 +1,11 @@
 package entities
 
+import (
+	"strconv"
+	"strings"
+	"time"
+)
+
 type Data struct {
 	PubKey            string `json:"pubkey"`
 	MacaroonHex       string `json:"macaroon_hex"`
@@ -7,4 +13,21 @@ type Data struct {
 	Endpoint          string `json:"endpoint"`
 	Tags              string `json:"tags,omitempty"`
 	ApiType           *int   `json:"api_type,omitempty"`
+}
+
+type JsonTime time.Time
+
+func (t JsonTime) MarshalJSON() ([]byte, error) {
+	return []byte(strconv.FormatInt(time.Time(t).Unix(), 10)), nil
+}
+
+func (t *JsonTime) UnmarshalJSON(s []byte) (err error) {
+	r := strings.Replace(string(s), `"`, ``, -1)
+
+	q, err := strconv.ParseInt(r, 10, 64)
+	if err != nil {
+		return err
+	}
+	*(*time.Time)(t) = time.Unix(q, 0)
+	return
 }
