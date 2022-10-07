e07fa6a6493de75b1be6dd6155597eba7607cd92
Gregor Pogacnik <gregor@bolt.observer>
2022-10-07 14:03:11 +0200

    feat: optional cert verification type added

--- entities/entities.go
+++ entities/entities.go
@@ -7,12 +7,13 @@ import (
 )
 
 type Data struct {
-	PubKey            string `json:"pubkey"`
-	MacaroonHex       string `json:"macaroon_hex"`
-	CertificateBase64 string `json:"certificate_base64,omitempty"`
-	Endpoint          string `json:"endpoint"`
-	Tags              string `json:"tags,omitempty"`
-	ApiType           *int   `json:"api_type,omitempty"`
+	PubKey               string `json:"pubkey"`
+	MacaroonHex          string `json:"macaroon_hex"`
+	CertificateBase64    string `json:"certificate_base64,omitempty"`
+	Endpoint             string `json:"endpoint"`
+	Tags                 string `json:"tags,omitempty"`
+	ApiType              *int   `json:"api_type,omitempty"`
+	CertVerificationType *int   `json:"cert_verification_type,omitempty"`
 }
 
 type JsonTime time.Time
